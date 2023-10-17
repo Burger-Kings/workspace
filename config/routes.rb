75b033713a3baeb19c13dd325dd4bc4c7db80916
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-10-17 02:46:39 +0000

    [Add]ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,32 @@ Rails.application.routes.draw do
 devise_for :admin,skip: [:passwords, :registrations], controllers: {
   sessions: "admin/sessions"
 }
+
+  namespace :admin do
+    get "/" => 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
+  scope module: :public do
+    root "homes#top"
+    get "about" => "homes#about"
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:update, :index, :create, :destroy]
+    delete "cart_items/destroy_all" => "cart_items#destroy_all"
+    resources :orders, only: [:new, :index, :create, :show]
+    post "orders/confirm" => "orders#confirm"
+    get "orders/complete" => "orders#complete"
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
+    get "customers/mypage" => "customers#show"
+    get "customers/information/edit" => "customers#edit"
+    patch "customers/mypage" => "customers#update"
+    get "customers/confirm_withdraw" => "customers#confirm_withdraw"
+    patch "customers/withdraw" => "customers#withdraw"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
