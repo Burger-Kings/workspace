4811e4c0aa21fbde0ae84b801951a9411eeafce9
iriehayato <143862129+iriehayato12@users.noreply.github.com>
2023-10-17 07:10:41 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ devise_for :admin,skip: [:passwords, :registrations], controllers: {
     resources :orders, only: [:new, :index, :create, :show]
     post "orders/confirm" => "orders#confirm"
     get "orders/complete" => "orders#complete"
-    resources :addresses, only: [:index, :edit, :create, :update, :destroi]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
 
     get "customers/mypage" => "customers#show"
     get "customers/information/edit" => "customers#edit"
