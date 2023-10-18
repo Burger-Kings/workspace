d06180aa369d9cedb8acd158007baf460d5e6a35
iriehayato <143862129+iriehayato12@users.noreply.github.com>
2023-10-18 07:03:45 +0000

    Add address,order関係

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,30 @@
+class Public::OrdersController < ApplicationController
+
+def new
+end
+
+def index
+ @orders = current_customer.orders
+end
+
+def show
+ @order = Order.find(params[:id])
+ @order_detail = @order.order_details
+end
+
+def create
+end
+
+def confirm
+end
+
+def complete
+end
+
+private
+
+def order_params
+params.require(:order).permit(:customer_id, :name, :postal_code, :address, :status, :shipping_fee, :total_price, :payment_method)
+end
+
+end
