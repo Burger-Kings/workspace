44752c3da7e66f58cd8363bc7549a68f747a1344
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-10-19 02:50:06 +0000

    get_item_imageメソッド修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,8 @@ class Item < ApplicationRecord
 
   def get_item_image(width, height)
     unless item_image.attached?
-      file_path = Rails.join('app/assets/images/no-image.png')
-      item_image_attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+      file_path = Rails.root.join('app/assets/images/no-image.png')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
     end
     item_image.variant(resize_to_limit: [width, height]).processed
   end
