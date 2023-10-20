8e669c7e5089737673af821226930ae24038f0c6
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-10-20 07:57:00 +0000

    Add: order controller追記　ビュー作成、　order_details、order model にenum追記　

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,5 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    @order_details = Oder.oder_details.all
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,22 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.find(params[:id])
+    @order_details = @order.order_details.all
+    @tax =1.1
   end
 
   def update
-
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    if @order.update(order_params)
+      @order_details.update_all(making_status: "制作待ち") if @order.status == "入金確認"
+    end
+    redirect_to admin_order_path(@order)
   end
-  
+
+  private
+
+  def order_params
+    params.require(:order).permit(:address, :name, :shipping_cost, :total_payment, :payment_method, :status)
+  end
 end
