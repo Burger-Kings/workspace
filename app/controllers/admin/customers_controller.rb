0f1119e1dce2cfc48bb3dba9271ecf8372c24d57
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-10-20 05:04:13 +0000

    admin/Oder controller作業中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,7 +14,7 @@ class Admin::CustomersController < ApplicationController
   def update
     customer = Customer.find(params[:id])
     customer.update(customer_params)
-    redirect_to admin_customer_path(customer.id)
+    redirect_to admin_customers_path
   end
 
   private
