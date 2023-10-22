db31c0db004a79df5c68a0da3f3a2df72513afc7
takuminakayama <143808505+zhongshanzuoxin@users.noreply.github.com>
2023-10-22 08:07:21 +0000

    トップ画面編集してます

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,5 @@
 class Public::HomesController < ApplicationController
   def top
-    @items = Item.all
     @genres = Genre.all
     @last_records = Item.order(created_at: :desc).limit(4)
   end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,7 @@
 class Public::ItemsController < ApplicationController
 
   def index
-    @items = Item.all
+    @items = Item.page(params[:page]).per(8)
     @items_count = @items.size
     @genres = Genre.all
   end
