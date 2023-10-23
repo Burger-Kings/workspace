a6b0bb47434ca889c04ac62d50b1aa905d5bb2fb
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-10-23 07:42:39 +0000

    devise,enum日本語化、エラーメッセージ日本語化、バリデーション追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
     @items = Item.page(params[:page])
   end
@@ -13,9 +15,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    item.save
-    redirect_to admin_items_path
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -23,9 +28,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to admin_item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # 会員でないユーザーの閲覧制限をどこまでかけるかを要検討
-  #before_action :authenticate_user!, except: [:top, :about]
+  #before_action :authenticate_admin!, except: [:top, :about]
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Admin)
       admin_path
@@ -24,5 +23,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana, :email,:postal_code, :address, :telephone_number, :is_active ])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   end
 end
