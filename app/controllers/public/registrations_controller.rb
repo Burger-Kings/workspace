6855e53263c3e9bd4f0ac9df7780dfa64fe5ce9e
takuminakayama <143808505+zhongshanzuoxin@users.noreply.github.com>
2023-10-24 16:13:28 +0000

    ビューの調整とか

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def after_sign_up_path_for(resource)
+    customers_mypage_path(resource.id)
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,10 +17,25 @@ Turbolinks.start()
 ActiveStorage.start()
 
 /* global $ */
-$(function() {
+$(document).on('turbolinks:load', function() {
   $('.images').slick({
-      autoplaySpeed: 4000,
+      autoplaySpeed: 3500,
       autoplay: true,
   });
 });
+
+
+$(document).ready(function() {
+  setTimeout(function() {
+    $('#ctn-preloader').addClass('loaded');
+    // Una vez haya terminado el preloader aparezca el scroll
+    if ($('#ctn-preloader').hasClass('loaded')) {
+      // Es para que una vez que se haya ido el preloader se elimine toda la seccion preloader
+      $('#preloader').delay(1000).queue(function() {
+        $(this).remove();
+      });
+    }
+  }, 4000);
+});
+
 
